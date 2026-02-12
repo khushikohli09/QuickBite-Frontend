0779a441f4ef930593737ed9440346842072c32a
khushikohli09 <157889269+khushikohli09@users.noreply.github.com>
2026-02-12 17:26:12 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: "https://quickbite-backend-47wd.onrender.com/api",
 });
 
 export default api;
+
